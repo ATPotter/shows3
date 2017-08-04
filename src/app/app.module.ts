aef4d204bae759ffda4b3bb3e08ba220fc28632b
Alan Potter <alan@thepotters.org>
2017-08-04 10:22:46 +0100

    First data service working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { HttpModule } from '@angular/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { TranslateModule } from '@ngx-translate/core';
 
+import { HttpClientModule } from '@angular/common/http';
+
+
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
@@ -12,18 +15,21 @@ import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 import { HomeModule } from './home/home.module';
 import { AboutModule } from './about/about.module';
+import { ShowsListModule } from './shows-list/shows-list.module';
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
+    HttpClientModule,
     TranslateModule.forRoot(),
     NgbModule.forRoot(),
     CoreModule,
     SharedModule,
     HomeModule,
     AboutModule,
+    ShowsListModule,
     AppRoutingModule
   ],
   declarations: [AppComponent],
